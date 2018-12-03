b2d79f35b4f29859212a3af6fde4c4884395ceb1
Nick Justin <Nicholasgjustin@gmail.com>
2018-12-03 15:37:11 -0600

    Added an error message to yearOfBirth

--- functionDrills.js
+++ functionDrills.js
@@ -1,12 +1,20 @@
-function createGreeting (name, age) {  
+function createGreeting(name, age) {
     return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth(age)}`;
 
 }
 
 const nickAndHisAge = createGreeting('Nick', 28);
-function yearOfBirth (age) {
-   return 2018 - age;  
+function yearOfBirth(age) {
+    if (age < 0 || typeof age !== 'number'){
+        throw new Error("Age cannot be below 0 and must be a number"); 
+    } else
+    return 2018 - age;
 }
 
-console.log(greeting);
 
+
+try {
+ createGreeting('Nick', -5);
+} catch(e) {
+    console.log(e);
+}
