20e872220214224e949ff52fe1ada032091d7295
Evan Guirino <etg624@gmail.com>
2018-12-03 13:07:41 -0800

    The strugs

--- functionDrills.js
+++ functionDrills.js
@@ -1,9 +1,12 @@
-function createGreeting (name, age) {    
-    const yearOfBirth = 2018 - age;
-    return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}`;
+function createGreeting (name, age) {  
+    return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth(age)}`;
+
 }
 
-const greeting = createGreeting();
+const nickAndHisAge = createGreeting('Nick', 28);
+function yearOfBirth (age) {
+   return 2018 - age;  
+}
 
 console.log(greeting);
 
